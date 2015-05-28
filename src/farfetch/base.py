4d3cbbf91d65f8ebe771d52645488a7e5537644a
joamag <joamag@gmail.com>
2015-05-28 11:44:58 +0100

    support for building requests with token support

--- src/farfetch/base.py
+++ src/farfetch/base.py
@@ -45,6 +45,10 @@ BASE_URL = "https://publicapi.farfetch.com/v1/"
 """ The default base url to be used when no other
 base url value is provided to the constructor """
 
+AUTH_URL = "https://publicapi.farfetch.com/authentication"
+""" The complete url for the authentication process
+and retrieval of (authentication) token """
+
 class Api(
     appier.Api,
     product.ProductApi
@@ -52,4 +56,33 @@ class Api(
 
     def __init__(self, *args, **kwargs):
         appier.Api.__init__(self, *args, **kwargs)
-        self.base_url = kwargs.get("base_url", BASE_URL)
+        self.base_url = appier.conf("FF_BASE_URL", BASE_URL)
+        self.auth_url = appier.conf("FF_AUTH_URL", AUTH_URL)
+        self.client_id = appier.conf("FF_CLIENT_ID", None)
+        self.client_secret = appier.conf("FF_CLIENT_SECRET", None)
+        self.base_url = kwargs.get("base_url", self.base_url)
+        self.auth_url = kwargs.get("auth_url", self.auth_url)
+        self.client_id = kwargs.get("client_id", self.client_id)
+        self.client_secret = kwargs.get("client_secret", self.client_secret)
+        self.token = kwargs.get("token", None)
+
+    def build(self, method, url, headers, kwargs):
+        auth = kwargs.get("auth", True)
+        if auth: headers["Authentication"] = self.get_token()
+
+    def get_token(self):
+        if self.token: return self.token
+        contents = self.post(
+            self.auth_url,
+            data_j = dict(
+                grant_type = "client_credentials",
+                scope = "api",
+                client_id = self.client_id,
+                client_secret = self.client_secret
+            ),
+            auth = False
+        )
+        token_type = contents["token_type"]
+        access_token = contents["access_token"]
+        self.token = "%s %s" % (token_type, access_token)
+        return self.token
